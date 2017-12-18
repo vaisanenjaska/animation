30010a63b73aaaccb61173dbf59a9a01793f1a9e
vaisanenjaska <vaisanenjaska@gmail.com>
2017-12-19 01:03:00 +0200

    Added a procfile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { IntlProvider, addLocaleData } from "react-intl";
 import fi from "react-intl/locale-data/fi";
@@ -16,4 +15,3 @@ ReactDOM.render(
   </IntlProvider>,
   document.getElementById("root")
 );
-registerServiceWorker();
